5031ba9407323534079b5e5f49ab93eda9dd9ee4
Jai Shah <jai.s@somaiya.edu>
2023-12-03 07:23:55 -0800

    50. Pow(x, n) [MEDIUM]

--- 162. Find Peak Element [MEDIUM]/find_peak_element.py
+++ 162. Find Peak Element [MEDIUM]/find_peak_element.py
@@ -9,48 +9,66 @@ find a peak element and return its index.
 The array may contain multiple peaks, in that case
 return the index to any one of the peaks is fine.
 You may imagine that num[-1] = num[n] = -∞.
-
-### 2. Solution Explanation:
-----------------------------
-If array has 3 or more elements, return middle if it is a peak
-Else search for peak on the side which has bigger neighbor of middle indexed element.
-If array has < 3 elements, return index of greater.
-
-### 3. Complexity Analysis:
-----------------------------
-Time - O(LogN)
-Space - O(1)
 """
 from typing import List
 
 
-class Solution(object):
+class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
+        # return self.linear_search_brute_force(nums)
+        return self.binary_search_peak_iterative(nums)
+        # return self.binary_search_peak_recursive(nums)
+
+    def linear_search_brute_force(self, nums: List[int]) -> int:
+        """
+        Linear search the array. Compare teh current element with it's neighbor to identify the peak
+
+        Time - O(N)
+        Space - O(1)
+        """
+        if len(nums) == 1: return 0
+        if nums[0] > nums[1]: return 0
+
+        for i in range(1, len(nums) - 1):
+            prev_num, cur_num, nxt_num = nums[i - 1], nums[i], nums[i + 1]
+            if cur_num > prev_num and cur_num > nxt_num:
+                return i
+
+        if nums[-1] > nums[-2]: return len(nums) - 1
+
+    def binary_search_peak_iterative(self, nums: List[int]) -> int:
+        """
+        If array has 3 or more elements, return middle if it is a peak
+        Else search for peak on the side which has bigger neighbor of middle indexed element.
+        If array has < 3 elements, return index of greater.
+
+        Time - O(LogN)
+        Space - O(1)
+        """
         left, right = 0, len(nums) - 1
 
-        while left < right - 1:  # at least 3 elements
-
-            mid = (left + right) // 2
-
-            if nums[mid] >= nums[mid + 1] and nums[mid] >= nums[mid - 1]:
-                return mid
-            if nums[mid + 1] > nums[mid]:  # RHS is higher (LHS could be also but arbitrarily choose RHS)
-                left = mid + 1
-            else:  # LHS must be higher if RHS is not
-                right = mid - 1
+        while left < right - 1:
+            middle = (left + right) // 2
+            if nums[middle] >= nums[middle - 1] and nums[middle] >= nums[middle + 1]:
+                return middle
+            elif nums[middle - 1] > nums[middle]:
+                right = middle - 1
+            else:
+                left = middle + 1
 
         if nums[left] >= nums[right]:
             return left
         return right
 
+    def binary_search_peak_recursive(self, nums, left, right):
+        """
+        If array has 3 or more elements, return middle if it is a peak
+        Else search for peak on the side which has bigger neighbor of middle indexed element.
+        If array has < 3 elements, return index of greater.
 
-class Solution1:
-    def findPeakElement(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return 0
-        return self.binary_search(nums, 0, len(nums) - 1)
-
-    def binary_search(self, nums, left, right):
+        Time - O(LogN)
+        Space - O(1)
+        """
         if left > right:
             return
 
@@ -60,7 +78,7 @@ class Solution1:
                 ((mid > 0) and (mid < len(nums) - 1) and (nums[mid] > nums[mid + 1]) and (nums[mid] > nums[mid - 1]))
                 or ((mid == 0) and (nums[mid] > nums[mid + 1]))
                 or ((mid == len(nums) - 1) and (nums[mid] > nums[mid - 1]))
-            ):
+        ):
             return mid
 
         elif nums[mid] < nums[mid + 1]:
---
+++ 50. Pow(x, n) [MEDIUM]/pow_x_n.py
@@ -0,0 +1,75 @@
+"""
+https://leetcode.com/problems/powx-n/
+
+### 1. Question Explanation:
+----------------------------
+Implement "pow(x, n)", which calculates "x" raised to the power "n" (i.e., x^n).
+
+#### Example 1:
+Input: x = 2.10000, n = 3
+Output: 9.26100
+
+#### Example 2:
+Input: x = 2.00000, n = -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+"""
+
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        # return self.brute_force(x, n)
+        # return self.binary_exponential_iterative(x, n)
+        return self.binary_exponential_recursive(x, n)
+
+    def brute_force(self, x: float, n: int) -> float:
+        """
+        Time - O(N)
+        Space - O(1)
+        """
+        pow_ans = 1
+        for i in range(n):
+            pow_ans = pow_ans * x
+        return pow_ans
+
+    def binary_exponential_recursive(self, x: float, n: int) -> float:
+        """
+        Time - O(LogN)
+        Space - O(1)
+        """
+        if n == 0: return 1
+
+        if n % 2 == 0:
+            temp = self.binary_exponential_recursive(x, n // 2)
+            return temp * temp
+        else:
+            if n > 0:
+                temp = self.binary_exponential_recursive(x, (n - 1) // 2)
+                return x * temp * temp
+            else:
+                temp = self.binary_exponential_recursive(x, (n + 1) // 2)
+                return (temp * temp) / x
+
+    def binary_exponential_iterative(self, x: float, n: int) -> float:
+        """
+        Time - O(LogN)
+        Space - O(1)
+        """
+        if n == 0: return 1
+
+        # Handle case where, n < 0.
+        if n < 0:
+            n = -1 * n
+            x = 1.0 / x
+
+        # Perform Binary Exponentiation.
+        result = 1
+        while n != 0:
+            # If 'n' is odd we multiply result with 'x' and reduce 'n' by '1'.
+            if n % 2 == 1:
+                result *= x
+                n -= 1
+            # We square 'x' and reduce 'n' by half, x^n => (x^2)^(n/2).
+            x *= x
+            n //= 2
+        return result
