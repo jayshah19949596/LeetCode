c98d99f6845842654a33323238f48965850298d5
Jai Shah <jai.s@somaiya.edu>
2023-12-03 17:02:17 -0800

    249. Group Shifted Strings [MEDIUM]

---
+++ 249. Group Shifted Strings [MEDIUM]/group_shifted_strings.py
@@ -0,0 +1,59 @@
+"""
+https://leetcode.com/problems/group-shifted-strings/
+
+### 1. Question Explanation:
+----------------------------
+We can shift a string by shifting each of its letters to its successive letter.
+
+For example, "abc" can be shifted to be "bcd".
+We can keep shifting the string to form a sequence.
+
+For example, we can keep shifting "abc" to form the sequence: "abc" -> "bcd" -> ... -> "xyz".
+Given an array of string, group all strings[i] that belong to the same shifting sequence.
+You may return the answer in any order.
+
+#### Example 1:
+Input: strings = ["abc","bcd","acef","xyz","az","ba","a","z"]
+Output: [["acef"],["a","z"],["abc","bcd","xyz"],["az","ba"]]
+
+#### Example 2:
+Input: strings = ["a"]
+Output: [["a"]]
+
+
+
+### 2. Solution Explanation:
+----------------------------
+For all string, keep on shifting the letters till the first character is letter "a".
+Group the strings with the sequence where first character is letter "a".
+Return the grouped strings as list.
+
+### 3. Complexity Analysis:
+----------------------------
+Time Complexity - O(N)
+Space Complexity - O(N)
+"""
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def shift_string_till_first_char_a(self, original_string):
+        char_a = "a"
+        if original_string[0] == char_a: return original_string
+        intermediate_string = original_string
+        while True:
+            intermedate_string_list = []
+            for char in intermediate_string:
+                if ord(char) == 122: intermedate_string_list.append(char_a)
+                else: intermedate_string_list.append(chr(ord(char)+1))
+            intermediate_string = "".join(intermedate_string_list)
+            if intermediate_string[0] == char_a: break
+        return intermediate_string
+
+    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        self.grouping = defaultdict(list)
+        for original_string in strings:
+            sequence_starting_with_a = self.shift_string_till_first_char_a(original_string)
+            self.grouping[sequence_starting_with_a].append(original_string)
+        return [self.grouping[key] for key in self.grouping]
