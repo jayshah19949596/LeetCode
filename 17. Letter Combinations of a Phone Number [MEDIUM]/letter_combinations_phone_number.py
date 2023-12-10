1d0977af9397763562e8e3c1c0d9b862a4046865
Jai Shah <jai.s@somaiya.edu>
2023-12-09 16:20:39 -0800

    17. Letter Combinations of a Phone Number [MEDIUM]

---
+++ 17. Letter Combinations of a Phone Number [MEDIUM]/letter_combinations_phone_number.py
@@ -0,0 +1,75 @@
+"""
+https://leetcode.com/problems/letter-combinations-of-a-phone-number
+
+### 1. Question Explanation:
+----------------------------
+Given a string containing digits from 2-9 inclusive,
+return all possible letter combinations that the number could represent. Return the answer in any order.
+A mapping of digits to letters (just like on the telephone buttons) is given below.
+Note that 1 does not map to any letters.
+
+
+#### Example 1:
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+#### Example 2:
+Input: digits = ""
+Output: []
+
+#### Example 3:
+Input: digits = "2"
+Output: ["a","b","c"]
+
+### 2. Solution Explanation:
+----------------------------
+APPROACH: DFS approach to get all letter combinations for a phone number
+
+### 3. Complexity Analysis:
+----------------------------
+Time Complexity: O((4^N)*N)
+Space Complexity: O(N)
+"""
+from typing import List
+
+
+class Solution:
+    digit2letter = {
+        "2": ["a", "b", "c"],
+        "3": ["d", "e", "f"],
+        "4": ["g", "h", "i"],
+        "5": ["j", "k", "l"],
+        "6": ["m", "n", "o"],
+        "7": ["p", "q", "r", "s"],
+        "8": ["t", "u", "v"],
+        "9": ["w", "x", "y", "z"],
+
+    }
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        # return self.recurse_dfs(digits, 0, [], [])
+        return self.iterative_dfs(digits)
+
+    def iterative_dfs(self, digits):
+        if not digits: return []
+        results = [[]]
+
+        for digit in digits:
+            new_results = []
+            for letter in Solution.digit2letter[digit]:
+                for result in results:
+                    new_results.append(result + [letter])
+            results = new_results
+
+        return ["".join(result) for result in results]
+
+    def recurse_dfs(self, digits, idx, results, intermediate_answer):
+        if idx == len(digits):
+            final_answer = "".join(intermediate_answer)
+            if final_answer: results.append(final_answer)
+            return results
+
+        for letter in Solution.digit2letter[digits[idx]]:
+            self.recurse_dfs(digits, idx + 1, results, intermediate_answer + [letter])
+
+        return results
