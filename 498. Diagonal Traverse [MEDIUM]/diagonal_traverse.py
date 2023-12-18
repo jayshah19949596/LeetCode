ea0e967f85d392694df8bedac90beb0186e74e41
Jai Shah <jai.s@somaiya.edu>
2023-12-18 07:07:20 -0800

    Changed diagonal traverse explanation.

--- 498. Diagonal Traverse [MEDIUM]/diagonal_traverse.py
+++ 498. Diagonal Traverse [MEDIUM]/diagonal_traverse.py
@@ -20,40 +20,42 @@ class Solution:
     def findDiagonalOrder(self, matrix: List[List[int]]) -> List[int]:
 
         if not matrix or not matrix[0]: return []  # Check for an empty matrix
-        N, M = len(matrix), len(matrix[0])  # The dimensions of the matrix
+        N, M = len(matrix), len(matrix[0])  #  The dimensions of the matrix
         row, column = 0, 0  # Indices that will help us progress through the matrix, one element at a time.
         direction = 1  # Variable to track what direction we are processing the current diagonal
         result = []  # Final result array that will contain all the elements of the matrix
 
         while row < N and column < M:
 
-            result.append(matrix[row][column])  # First and foremost, add the current element to the result matrix.
+            result.append(matrix[row][column]) # First and foremost, add the current element to the result matrix.
 
             # Move along in the current diagonal depending upon the current direction.
-            # if going UPWARDS: [i, j] -> [i - 1, j + 1] .
-            # if going DOWNWARDS: [i, j] -> [i + 1][j - 1] .
+            # [i, j] -> [i - 1, j + 1] if going up
+            # [i, j] -> [i + 1][j - 1] if going down.
             new_row = row + (-1 if direction == 1 else 1)
             new_column = column + (1 if direction == 1 else -1)
 
-            # Checking if the next element in the diagonal is within the bounds of the matrix or not.
-            # If it's not within the bounds, we have to find the next head.
+            # Checking if the next element in the diagonal is within the
+            # bounds of the matrix or not. If it's not within the bounds,
+            # we have to find the next head.
             if new_row < 0 or new_row == N or new_column < 0 or new_column == M:
 
-                # If the current diagonal was going in the upwards direction.
                 if direction:
+                    # If the current diagonal was going in the upwards direction.
                     # For an upwards going diagonal having [i, j] as its tail
                     # If [i, j + 1] is within bounds, then it becomes the next head.
-                    # Otherwise, the element directly below i.e. the element [i + 1, j] becomes the next head.
+                    # Otherwise, the element directly below i.e. the element [i + 1, j] becomes the next head
                     row += (column == M - 1)
                     column += (column < M - 1)
                 else:
+                    # Else the current diagonal was going in the downwads direction.
                     # For a downwards going diagonal having [i, j] as its tail
                     # If [i + 1, j] is within bounds, then it becomes the next head.
-                    # Otherwise, the element directly below i.e. the element [i, j + 1] becomes the next head.
+                    # Otherwise, the element directly above i.e. the element [i, j + 1] becomes the next head
                     column += (row == N - 1)
                     row += (row < N - 1)
 
-                direction = 1 - direction  # Flip the direction
+                direction = 1 - direction # Flip the direction
             else:
                 row = new_row
                 column = new_column
