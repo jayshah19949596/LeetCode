2309d05105a198ec58b05f42e20c0c81e31e72d0
Jai Shah <jaishah@amazon.com>
2023-12-25 11:04:14 -0800

    1091. Shortest Path in Binary Matrix, added dijkstras and A* algorithm solution

---
+++ 1091. Shortest Path in Binary Matrix [MEDIUM]/shortest_path_in_binary_matrix_A-star.py
@@ -0,0 +1,46 @@
+from typing import List
+import heapq
+import math
+
+
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        total_moves = self.a_star_search(grid)
+        if total_moves == float("inf"):
+            return -1
+        else:
+            return total_moves
+
+    @staticmethod
+    def get_estimate_dist(x, y):
+        return max(y[0]-x[0], y[1]-x[1])
+
+    def a_star_search(self, grid):
+        min_moves = float("inf")
+        n, m = len(grid)-1, len(grid[0])-1
+        if grid[0][0] == 1 or grid[-1][-1] == 1: return min_moves
+
+        neighboring_rows = [0, 1, -1, 0, -1, 1, -1, 1]
+        neighboring_cols = [1, 0, 0, -1, -1, 1, 1, -1]
+        queue = [ [Solution.get_estimate_dist([0, 0], [n, m]), 1, 0, 0] ]
+        visited = set()
+
+        while queue:
+            cur_dist, moves, cur_row, cur_col = heapq.heappop(queue)
+            if moves>min_moves:
+                continue
+
+            if cur_row == len(grid) - 1 and cur_col == len(grid[0]) - 1:
+                min_moves = min(min_moves, moves)
+                continue
+
+            if (cur_row, cur_col) in visited: continue
+            visited.add((cur_row, cur_col))
+
+            for neighbor_row, neighbor_col in zip(neighboring_rows, neighboring_cols):
+                next_row, next_col = cur_row + neighbor_row, cur_col + neighbor_col
+                if next_row >= 0 and next_row < len(grid) and next_col >= 0 and next_col < len(grid[0]) and \
+                        grid[next_row][next_col] == 0:
+                    hueristic = moves+Solution.get_estimate_dist([cur_row + neighbor_row, cur_col + neighbor_col], [n, m])
+                    heapq.heappush(queue, [hueristic, moves+1, cur_row + neighbor_row, cur_col + neighbor_col])
+        return min_moves
---
+++ 1091. Shortest Path in Binary Matrix [MEDIUM]/shortest_path_in_binary_matrix_dijkstras.py
@@ -0,0 +1,41 @@
+from typing import List
+import heapq
+import math
+
+
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        total_moves = self.dijkstras_search(grid)
+        if total_moves == float("inf"):
+            return -1
+        else:
+            return total_moves
+
+    def dijkstras_search(self, grid):
+        min_moves = float("inf")
+        n, m = len(grid)-1, len(grid[0])-1
+        if grid[0][0] == 1 or grid[-1][-1] == 1: return min_moves
+
+        neighboring_rows = [0, 1, -1, 0, -1, 1, -1, 1]
+        neighboring_cols = [1, 0, 0, -1, -1, 1, 1, -1]
+        queue = [ [1, 0, 0] ]
+        visited = set()
+
+        while queue:
+            moves, cur_row, cur_col = heapq.heappop(queue)
+            if moves>min_moves:
+                continue
+
+            if cur_row == len(grid) - 1 and cur_col == len(grid[0]) - 1:
+                min_moves = min(min_moves, moves)
+                continue
+
+            if (cur_row, cur_col) in visited: continue
+            visited.add((cur_row, cur_col))
+
+            for neighbor_row, neighbor_col in zip(neighboring_rows, neighboring_cols):
+                next_row, next_col = cur_row + neighbor_row, cur_col + neighbor_col
+                if next_row >= 0 and next_row < len(grid) and next_col >= 0 and next_col < len(grid[0]) and \
+                        grid[next_row][next_col] == 0:
+                    heapq.heappush(queue, [moves+1, cur_row + neighbor_row, cur_col + neighbor_col])
+        return min_moves
