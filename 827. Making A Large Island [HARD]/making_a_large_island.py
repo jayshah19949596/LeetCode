da2a1c1f6569080bc2298f6b5ff373737c29f6d4
Jai Shah <jaishah@amazon.com>
2023-12-25 13:41:43 -0800

    827. Making A Large Island [HARD]

---
+++ 827. Making A Large Island [HARD]/making_a_large_island.py
@@ -0,0 +1,89 @@
+
+"""
+============================
+APPROACH-1: Brute Force DFS
+===========================
+"""
+class Solution(object):
+    def largestIsland(self, grid):
+        max_area = 0
+        rows, cols = len(grid) - 1, len(grid[0]) - 1
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1:
+                    area = self.iterative_dfs(grid, r, c, rows, cols)
+                else:
+                    grid[r][c] = 1
+                    area = self.iterative_dfs(grid, r, c, rows, cols)
+                    grid[r][c] = 0
+                max_area = max(max_area, area)
+        return max_area
+
+    def iterative_dfs(self, grid, r, c, rows, cols):
+        area = 1
+        visited = set([(r, c)])
+        neighbor_rows = [0, 1, 0, -1]
+        neighbor_cols = [1, 0, -1, 0]
+        stack = [(r, c)]
+
+        while stack:
+            cur_r, cur_c = stack.pop()
+            for nr, nc in zip(neighbor_rows, neighbor_cols):
+                new_r, new_c = cur_r + nr, cur_c + nc
+                if rows >= new_r >= 0 and cols >= new_c >= 0 and grid[new_r][new_c] == 1:
+                    if (new_r, new_c) not in visited:
+                        visited.add((new_r, new_c))
+                        area += 1
+                        stack.append((new_r, new_c))
+
+        return area
+
+
+"""
+============================
+APPROACH-2: Component Sizes
+============================
+"""
+class Solution(object):
+    def largestIsland(self, grid):
+        area_map = {}
+        max_area = 0
+        rows, cols = len(grid) - 1, len(grid[0]) - 1
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1:
+                    self.iterative_dfs(grid, r, c, rows, cols, area_map)
+                    max_area = max(max_area, area_map[(r, c)])
+
+        neighbor_rows = [0, 1, 0, -1]
+        neighbor_cols = [1, 0, -1, 0]
+
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 0:
+                    area = 1
+                    for nr, nc in zip(neighbor_rows, neighbor_cols):
+                        area += area_map[(nr, nc)]
+                    max_area = max(max_area, area)
+
+        return max_area
+
+    def iterative_dfs(self, grid, r, c, rows, cols, area_map):
+        area = 1
+        visited = set([(r, c)])
+        neighbor_rows = [0, 1, 0, -1]
+        neighbor_cols = [1, 0, -1, 0]
+        stack = [(r, c)]
+
+        while stack:
+            cur_r, cur_c = stack.pop()
+            for nr, nc in zip(neighbor_rows, neighbor_cols):
+                new_r, new_c = cur_r + nr, cur_c + nc
+                if rows >= new_r >= 0 and cols >= new_c >= 0 and grid[new_r][new_c] == 1:
+                    if (new_r, new_c) not in visited:
+                        visited.add((new_r, new_c))
+                        area += 1
+                        stack.append((new_r, new_c))
+        for r, c in visited:
+            area_map[(r, c)] = area
+        return area
