52f59eee8ec111a4ce20a68ff7bf911373ac2ea4
Jai Shah <jaishah@amazon.com>
2023-12-25 13:44:11 -0800

    827. Making A Large Island [HARD]

--- 827. Making A Large Island [HARD]/making_a_large_island.py
+++ 827. Making A Large Island [HARD]/making_a_large_island.py
@@ -45,6 +45,11 @@ APPROACH-2: Component Sizes
 ============================
 """
 class Solution(object):
+    def get_neighbors(self):
+        neighbor_rows = [0, 1, 0, -1]
+        neighbor_cols = [1, 0, -1, 0]
+        return zip(neighbor_rows, neighbor_cols)
+
     def largestIsland(self, grid):
         area_map = {}
         max_area = 0
@@ -52,18 +57,20 @@ class Solution(object):
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 if grid[r][c] == 1:
-                    self.iterative_dfs(grid, r, c, rows, cols, area_map)
-                    max_area = max(max_area, area_map[(r, c)])
-
-        neighbor_rows = [0, 1, 0, -1]
-        neighbor_cols = [1, 0, -1, 0]
+                    if (r, c) not in area_map:
+                        self.iterative_dfs(grid, r, c, rows, cols, area_map)
+                        max_area = max(max_area, area_map[(r, c)][1])
 
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 if grid[r][c] == 0:
-                    area = 1
-                    for nr, nc in zip(neighbor_rows, neighbor_cols):
-                        area += area_map[(nr, nc)]
+                    visisted, area = set([]), 1
+                    for nr, nc in self.get_neighbors():
+                        if (r + nr, c + nc) in area_map:
+                            island_id, island_area = area_map[(r + nr, c + nc)]
+                            if island_id not in visisted:
+                                visisted.add(island_id)
+                                area += island_area
                     max_area = max(max_area, area)
 
         return max_area
@@ -77,13 +84,15 @@ class Solution(object):
 
         while stack:
             cur_r, cur_c = stack.pop()
-            for nr, nc in zip(neighbor_rows, neighbor_cols):
+            for nr, nc in self.get_neighbors():
                 new_r, new_c = cur_r + nr, cur_c + nc
                 if rows >= new_r >= 0 and cols >= new_c >= 0 and grid[new_r][new_c] == 1:
                     if (new_r, new_c) not in visited:
                         visited.add((new_r, new_c))
                         area += 1
                         stack.append((new_r, new_c))
-        for r, c in visited:
-            area_map[(r, c)] = area
+
+        island_id = len(area_map) + 1
+        for row, col in visited:
+            area_map[(row, col)] = [island_id, area]
         return area
