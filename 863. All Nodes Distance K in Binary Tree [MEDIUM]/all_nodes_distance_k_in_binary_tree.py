ff69dc0a02c6184d23e9f3a8cbf9dfb6afcc90e1
Jai Shah <jaishah@amazon.com>
2023-12-27 22:01:46 -0800

    863. All Nodes Distance K in Binary Tree [MEDIUM]

---
+++ 863. All Nodes Distance K in Binary Tree [MEDIUM]/all_nodes_distance_k_in_binary_tree.py
@@ -0,0 +1,51 @@
+"""
+863. All Nodes Distance K in Binary Tree [MEDIUM]
+https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree
+
+### 1. Question Explanation:
+----------------------------
+Given the root of a binary tree, the value of a target node "target", and an integer "k".
+Return an array of the values of all nodes that have a distance k from the target node.
+
+### 2. Solution Explanation:
+----------------------------
+Intuition: Traverse from target node and see if the children or ancestrol nodes are distanced by value k
+Find Path from root to target in path.
+For all node in path traverse in dfs fashion to find nodes distanced with value k from target.
+
+### 3. Complexity Analysis:
+----------------------------
+Time Complexity: O(N): for visiting all nodes
+Space Complexity: O(N), for maintaining path variable from root to target node.
+"""
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        path, results, blocker = [], [], None
+        self.find_path_from_root_to_target(root, target, path)
+        for i in range(len(path)):
+            if i>0: blocker = path[i-1]
+            self.find_nodes_with_k_dist(path[i], k-i, results, blocker)
+        return results
+
+    def find_nodes_with_k_dist(self, node, dist, results, blocker):
+        if not node or node == blocker or dist<0: return
+        if dist == 0: results.append(node.val)
+        self.find_nodes_with_k_dist(node.left, dist-1, results, blocker)
+        self.find_nodes_with_k_dist(node.right, dist-1, results, blocker)
+
+    def find_path_from_root_to_target(self, node, target, path):
+        if not node:
+            return False
+        if node == target or self.find_path_from_root_to_target(node.left, target, path) or self.find_path_from_root_to_target(node.right, target, path):
+            path.append(node)
+            return True
+        return False
