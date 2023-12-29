740d27e54013a2d1240d527d0be364da597f96ac
Jai Shah <jaishah@amazon.com>
2023-12-29 08:22:22 -0800

    53. Maximum Subarray [MEDIUM] Solution explanation

--- 53. Maximum Subarray [MEDIUM]/maximum_subarray.py
+++ 53. Maximum Subarray [MEDIUM]/maximum_subarray.py
@@ -9,6 +9,10 @@ Given an integer array nums, find the subarray with the largest sum, and return 
 ### 2. Solution Explanation:
 ----------------------------
 Kadane’s Algorithm
+The idea of Kadane’s algorithm is to maintain a variable 'max_ending' that stores the maximum sum contiguous subarray ending at current index.
+Variable 'max_so_far' stores the maximum sum of contiguous subarray found so far,
+Everytime there is a positive-sum value in 'max_ending' compare it with 'max_so_far' and update 'max_so_far' if it is greater than 'max_so_far'.
+
 
 ### 3. Complexity Analysis:
 ----------------------------
