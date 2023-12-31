7ed9eb318bc7daf39d11346e42557481386804b1
Jai Shah <jaishah@amazon.com>
2023-12-30 21:38:44 -0800

    Reduce operations 766.Toeplitz Matrix

--- 766. Toeplitz Matrix [EASY]/toeplitz_matrix.py
+++ 766. Toeplitz Matrix [EASY]/toeplitz_matrix.py
@@ -16,11 +16,8 @@ from typing import List
 
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        if not matrix:
-            return False
-
-        for row in range(len(matrix)):
-            for col in range(len(matrix[0])):
-                if row > 0 and col > 0 and matrix[row][col] != matrix[row - 1][col - 1]:
+        for row in range(1, len(matrix)):
+            for col in range(1, len(matrix[0])):
+                if matrix[row][col] != matrix[row - 1][col - 1]:
                     return False
         return True
