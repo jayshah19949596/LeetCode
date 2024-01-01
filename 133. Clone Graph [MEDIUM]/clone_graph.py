77d7534470a09236e0ea62ae98a7d1587bb79913
Jai Shah <jaishah@amazon.com>
2024-01-01 10:13:24 -0800

    133. Clone Graph [MEDIUM]

---
+++ 133. Clone Graph [MEDIUM]/clone_graph.py
@@ -0,0 +1,50 @@
+"""
+133. Clone Graph [MEDIUM]
+https://leetcode.com/problems/clone-graph
+
+### 1. Question:
+----------------------------
+Given a reference of a node in a connected undirected graph.
+Return a deep copy (clone) of the graph.
+
+### 2. Solution:
+----------------------------
+Iterative DFS
+
+### 3. Complexity Analysis:
+----------------------------
+Time Complexity: O(V+E), V is a number of nodes (vertices) and E is a number of edges
+Space Complexity: O(V)
+"""
+from typing import Optional
+
+
+class Node:
+    def __init__(self, val=0, neighbors=None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node: return None
+
+        visited = {node: Node(node.val)}
+        stack = [node]
+
+        while stack:
+            cur_node = stack.pop()
+            cur_copy_node = visited[cur_node]
+            copy_node_neighbors = []
+
+            for neighbor_node in cur_node.neighbors:
+                if neighbor_node not in visited:
+                    copy_neighbor_node = Node(neighbor_node.val)
+                    visited[neighbor_node] = copy_neighbor_node
+                    copy_node_neighbors.append(copy_neighbor_node)
+                    stack.append(neighbor_node)
+                else:
+                    copy_node_neighbors.append(visited[neighbor_node])
+            cur_copy_node.neighbors = copy_node_neighbors
+
+        return visited[node]
