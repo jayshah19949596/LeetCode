10b506ec5160567588da46317efbada48adff8ec
Jai Shah <jaishah@amazon.com>
2024-01-20 23:13:58 -0800

    2251. Number of Flowers in Full Bloom [HARD]

--- 2251. Number of Flowers in Full Bloom [HARD]/flower_bloom_binary_search_opimal.py
+++ 2251. Number of Flowers in Full Bloom [HARD]/flower_bloom_binary_search_opimal.py
@@ -41,14 +41,13 @@ from bisect import bisect_right
 
 class Solution:
     def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
-        starts, ends = [], []
+        starts, ends, ans = [], [], []
 
         for start, end in flowers:
             starts.append(start)
             ends.append(end + 1)
 
         starts.sort(), ends.sort()
-        ans = []
 
         for person in people:
             i = bisect_right(starts, person)
