66758e94cda3e80241af57585f13fd29239d9d79
Jai Shah <jaishah@amazon.com>
2024-02-05 20:50:16 -0800

    2484. Count Palindromic Subsequences [HARD]

---
+++ 2484. Count Palindromic Subsequences [HARD]/brute_force.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    def countPalindromes(self, s: str) -> int:
+        total_subseq = set([])
+
+        def recurse(inter, l, r):
+            nonlocal s
+            if len(inter) >= 5:
+                total_subseq.add(tuple(inter))
+                return
+            if l < 0 or r >= len(s): return
+
+            if s[l] == s[r]:
+                recurse([l] + inter + [r], l - 1, r + 1)
+            if l - 1 >= 0: recurse(inter[:], l - 1, r)
+            if r + 1 < len(s): recurse(inter[:], l, r + 1)
+
+        for i in range(2, len(s) - 2):
+            recurse([i], i - 1, i + 1)
+
+        return len(total_subseq)
