3b9ba178f3d3694afd82a38f7b0e0f7e1b542dec
jayshah19949596 <jayshah19949596@yahoo.in>
2024-09-15 14:20:40 -0700

    791. Custom Sort String [MEDIUM]

--- 791. Custom Sort String [MEDIUM]/custom-sort-string.py
+++ 791. Custom Sort String [MEDIUM]/custom-sort-string.py
@@ -19,22 +19,50 @@ Since "d" does not appear in order, it can be at any position in the returned st
 #### Example 2:
 Input: order = "cbafg", s = "abcd"
 Output: "cbad"
+"""
+from collections import Counter
 
-### 2. Solution Explanation:
+"""
+=============================================
+APPROACH-1: Custom Comparator with Sorting
+=============================================
+### 1. Solution Explanation:
+----------------------------
+Create Custom comparator to be used with Sorting.
+
+### 2. Complexity Analysis:
+----------------------------
+Time Complexity: O(N*LogN)
+Space Complexity: O(N)
+""""
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        custom_comparator_values = defaultdict(int)
+
+        for i, char in enumerate(order):
+            custom_comparator_values[char] = i
+
+        return ''.join(sorted(list(s), key=lambda x: custom_comparator_values[x]))
+
+"""
+=============================================
+APPROACH-2: Frequency Table and Counting
+=============================================
+1. We count the elements of "s" in a frequency table.
+2. We write the elements of "order" in the order they appear in "s".
+3. We write the elements of "s" we didn't write in any order.
+
+### 1. Solution Explanation:
 ----------------------------
 The trick is to count the elements of "s". After we have some count[char] = (the number of occurrences of char in "s").
 We can write these elements in the order we want. The order is "order" + (characters not in "order" in any sequence).
 
 
-### 3. Complexity Analysis:
+### 2. Complexity Analysis:
 ----------------------------
 Time Complexity: O(order.length+s.length), the time it takes to iterate through S and T
 Space Complexity: O(s.length). We count at most 26 different lowercase letters, but the final answer has the same length as T.
-
 """
-from collections import Counter
-
-
 class Solution:
 
     def customSortString(self, order: str, s: str) -> str:
@@ -50,5 +78,3 @@ class Solution:
                 results.append(char * s_counter[char])
 
         return "".join(results)
-
-
