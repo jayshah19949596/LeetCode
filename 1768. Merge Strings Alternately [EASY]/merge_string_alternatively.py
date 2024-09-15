45076df54283610ad72ee842af913a4327ead072
jayshah19949596 <jayshah19949596@yahoo.in>
2024-09-14 21:56:38 -0700

    1768. Merge Strings Alternately [EASY]

--- 1768. Merge Strings Alternately [EASY]/merge_string_alternatively.py
+++ 1768. Merge Strings Alternately [EASY]/merge_string_alternatively.py
@@ -1,3 +1,31 @@
+"""
+1768. Merge Strings Alternately
+https://leetcode.com/problems/merge-strings-alternately
+
+### 1. Question Explanation:
+----------------------------
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+
+#### Example 1:
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+
+### 2. Solution Explanation:
+----------------------------
+???
+
+### 3. Complexity Analysis:
+----------------------------
+Time - O(len(word1) + len(word2))
+Space - O(1)
+"""
 class Solution(object):
     def mergeAlternately(self, word1, word2):
         results = []
