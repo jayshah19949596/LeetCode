14177c5c009dd9ab91d622eadc37aff59e9ae4e4
jayshah19949596 <jayshah19949596@yahoo.in>
2024-09-15 23:45:33 -0700

    142. Linked List Cycle II [MEDIUM]

---
+++ 142. Linked List Cycle II [MEDIUM]/linked_list_cycle_ii.py
@@ -0,0 +1,72 @@
+"""
+142. Linked List Cycle II [MEDIUM]
+https://leetcode.com/problems/linked-list-cycle-ii
+
+### 1. Question Explanation:
+----------------------------
+Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+Do not modify the linked list.
+
+
+"""
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        storage = set([])
+        while head:
+            if head in storage:
+                return head
+            storage.add(head)
+            head = head.next
+
+
+class Solution(object):
+    def getIntersect(self, head):
+        tortoise = head
+        hare = head
+
+        # A fast pointer will either loop around a cycle and meet the slow
+        # pointer or reach the `null` at the end of a non-cyclic list.
+        while hare is not None and hare.next is not None:
+            tortoise = tortoise.next
+            hare = hare.next.next
+            if tortoise == hare:
+                return tortoise
+
+
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head is None:
+            return None
+
+        # If there is a cycle, the fast/slow pointers will intersect at some
+        # node. Otherwise, there is no cycle, so we cannot find an entrance to
+        # a cycle.
+        intersect = self.getIntersect(head)
+        if intersect is None:
+            return None
+
+        # To find the entrance to the cycle, we have two pointers traverse at
+        # the same speed -- one from the front of the list, and the other from
+        # the point of intersection.
+        ptr1 = head
+        ptr2 = intersect
+        while ptr1 != ptr2:
+            ptr1 = ptr1.next
+            ptr2 = ptr2.next
+
+        return ptr1
--- 725. Split Linked List in Parts [MEDIUM]/split_linked_list_in_parts.py
+++ 725. Split Linked List in Parts [MEDIUM]/split_linked_list_in_parts.py
@@ -1,8 +1,32 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+"""
+725. Split Linked List in Parts [MEDIUM]
+https://leetcode.com/problems/split-linked-list-in-parts/
+
+### 1. Question Explanation:
+----------------------------
+Given the head of a singly linked list and an integer k, split the linked list into k consecutive linked list parts.
+The length of each part should be as equal as possible: no two parts should have a size differing by more than one. This may lead to some parts being null.
+The parts should be in the order of occurrence in the input list, and parts occurring earlier should always have a size greater than or equal to parts occurring later.
+Return an array of the k parts.
+
+### 2. Solution Explanation:
+----------------------------
+We are given a linked list head and an integer k.
+We want to split head evenly into k equally sized parts and return an array of the k parts.
+If head cannot be split evenly, the sizes of the k parts can differ by at most 1, with the larger parts appearing before the smaller ones.
+
+
+
+### 3. Complexity Analysis:
+----------------------------
+Time - O(N)
+Space - O(N)
+"""
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
         ans = [None] * k
