4408b459c6b84fbd46c1a0340ecb0f61b070b619
Jay Shah <jaishah@amazon.com>
2024-09-17 22:07:05 -0700

    78. Subsets [MEDIUM]

--- 78. Subsets [MEDIUM]/subsets.py
+++ 78. Subsets [MEDIUM]/subsets.py
@@ -4,15 +4,15 @@ https://leetcode.com/problems/subsets-ii/description/
 
 ### 1. Question Explanation:
 ----------------------------
-Given an integer array nums that may contain duplicates, return all possible
+Given an integer array nums of unique elements, return all possible
 subsets (the power set).
-The solution set must not contain duplicate subsets. Return the solution in any order.
+The solution set must not contain duplicate subsets. Return the solution in any order
 
-Example 1:
-Input: nums = [1,2,2]
-Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
-Example 2:
+#### Example 1:
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
 
+#### Example 2:
 Input: nums = [0]
 Output: [[],[0]]
 """
@@ -28,18 +28,16 @@ Time: O(n⋅2**n)
 Space: O(n)
 """
 class Solution:
-    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        subsets = set([])
-        nums.sort()
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        subsets = []
         def recurse(arr, inter_arr, idx, n):
-            subsets.add(tuple(inter_arr[:]))
+            subsets.append(inter_arr[:])
             for i in range(idx, n):
                 inter_arr.append(arr[i])
                 recurse(arr, inter_arr, i+1, n)
                 inter_arr.pop()
         recurse(nums, [], 0, len(nums))
-        return list(subsets)
-
+        return subsets
 
 """
 ==========================
@@ -52,20 +50,11 @@ Time: O(n⋅2**n)
 Space: O(n)
 """
 class Solution:
-    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
         subsets = [[]]
-        nums.sort()
-
         for num in nums:
             new_subsets = []
             for subset in subsets:
                 new_subsets.append(subset+[num])
             subsets = new_subsets+subsets
-
-        result, seen = [], set([])
-        for subset in subsets:
-            subset_tuple = tuple(subset)
-            if subset_tuple not in seen:
-                seen.add(subset_tuple)
-                result.append(subset_tuple)
-        return result
+        return subsets
