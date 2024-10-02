352d3206850b945cbccdba07a6a53605f9b5c1d6
Jay Shah <jaishah@amazon.com>
2024-10-02 00:02:48 -0700

    Valid Palindrome Space Optimized

--- 125. Valid Palindrome [EASY]/valid-palindrome.py
+++ 125. Valid Palindrome [EASY]/valid-palindrome.py
@@ -44,3 +44,19 @@ class Solution:
             i += 1
             j -= 1
         return True
+
+"""
+SPACE OPTIMIZED
+"""
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i, j = 0, len(s) - 1
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while i < j and not s[j].isalnum():
+                j -= 1
+            if s[i].lower() != s[j].lower():
+                return False
+            i, j = i+1, j-1
+        return True
