17b40448244c0e400482759764dc4d44565790ed
Jay Shah <jaishah@amazon.com>
2024-10-03 21:33:58 -0700

    123. Best Time to Buy and Sell Stock III [HARD]

---
+++ 123. Best Time to Buy and Sell Stock III [HARD]/buy_and_sell_stock_iii.py
@@ -0,0 +1,24 @@
+"""
+123. Best Time to Buy and Sell Stock III [HARD]
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii
+
+Solution: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/editorial/
+"""
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        t1_cost, t2_cost = float("inf"), float("inf")
+        t1_profit, t2_profit = 0, 0
+
+        for price in prices:
+            # the maximum profit if only one transaction is allowed
+            t1_cost = min(t1_cost, price)
+            t1_profit = max(t1_profit, price - t1_cost)
+            # Reinvest the gained profit in the second transaction
+            t2_cost = min(t2_cost, price - t1_profit)
+            t2_profit = max(t2_profit, price - t2_cost)
+
+        return t2_profit
