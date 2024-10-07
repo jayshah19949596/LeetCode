79aecb3d28703e95f8eb8453683532af1c2799de
Jay Shah <jaishah@amazon.com>
2024-10-06 21:39:30 -0700

    6. Zigzag Conversion

--- 6. Zigzag Conversion [MEDIUM]/zigzag.py
+++ 6. Zigzag Conversion [MEDIUM]/zigzag.py
@@ -2,13 +2,17 @@ class Solution(object):
     def convert(self, s: str, num_rows: int) -> str:
         if num_rows == 1: return s
         row, direction = 0, -1
-        table = [[] for i in range(num_rows)]
+        zigzag = [[] for i in range(num_rows)]
 
         for i in range(len(s)):
 
             if row == 0 or row == num_rows - 1:
                 direction = -1 * direction
-            table[row].append(s[i])
+            zigzag[row].append(s[i])
             row = row + direction
 
-        return "".join([char for row in table for char in row])
+        results = []
+        for row in zigzag:
+            for char in row:
+                results.append(char)
+        return "".join(results)
