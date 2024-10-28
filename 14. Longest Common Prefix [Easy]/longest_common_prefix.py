37619add08eaf894ba0ad6db2ccc84232aaba6d6
Jai Shah <jai.s@somaiya.edu>
2024-10-27 20:55:12 -0700

    Update longest_common_prefix.py

--- 14. Longest Common Prefix [Easy]/longest_common_prefix.py
+++ 14. Longest Common Prefix [Easy]/longest_common_prefix.py
@@ -3,10 +3,10 @@ class Solution:
         results, char_idx = [], 0
 
         while char_idx<len(strs[0]):
-            
+            # Character to compare
             char = strs[0][char_idx]
             for i in range(1, len(strs)):
-                if char_idx>=len(strs[i]) or strs[i][char_idx] != char:
+                if char_idx==len(strs[i]) or strs[i][char_idx] != char:
                     return "".join(results)
             results.append(char)
             char_idx += 1
