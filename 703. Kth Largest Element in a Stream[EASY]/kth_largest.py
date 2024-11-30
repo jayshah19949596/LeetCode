1c783af2c7a16b2840fdb0a8abbc8f939235a294
Jai Shah <jai.s@somaiya.edu>
2024-11-29 22:13:40 -0800

    Create kth_largest.py

---
+++ 703. Kth Largest Element in a Stream[EASY]/kth_largest.py
@@ -0,0 +1,12 @@
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.min_heap = []
+        self.k = k
+        for num in nums:
+            self.add(num)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.min_heap, val)
+        if len(self.min_heap) > self.k:
+            heapq.heappop(self.min_heap)
+        return self.min_heap[0]
