dfb56b8551a2c854f7f7a1f0b73b70b01bb3a0da
Jai Shah <jai.s@somaiya.edu>
2024-12-01 16:16:10 -0800

    Update first_bad_version.py
    Change while condition and right = mid - 1

--- 278. First Bad Version [EASY]/first_bad_version.py
+++ 278. First Bad Version [EASY]/first_bad_version.py
@@ -14,10 +14,10 @@ SPACE COMPLEXITY: O(1)
 class Solution:
     def firstBadVersion(self, n: int) -> int:
         left, right = 1, n
-        while left < right:
+        while left <= right:
             mid = (left+right)// 2
             if isBadVersion(mid):
-                right = mid
+                right = mid - 1
             else:
                 left = mid + 1
         return int(left)
