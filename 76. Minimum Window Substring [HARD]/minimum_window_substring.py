0f92be4eff71d87b94e3656a914787d2c152d6cd
Jai Shah <jai.s@somaiya.edu>
2024-12-04 23:47:33 -0800

    Update minimum_window_substring.py

--- 76. Minimum Window Substring [HARD]/minimum_window_substring.py
+++ 76. Minimum Window Substring [HARD]/minimum_window_substring.py
@@ -20,35 +20,38 @@ Space - O(m)
 """
 from collections import Counter
 
-
+# Find all indices in S matching the first letter of T and create a list of windows. 
+# For each letter of T, for each window find the next letter of S that matches the letter and extend the window. 
+# If the letter is not found, remove the window. Return the smallest window.
+# Time - O(mn), worst case when all letters are identical means len(S) windows
+# Space - O(m)
 class Solution(object):
     def minWindow(self, s: str, t: str) -> str:
         if len(s) < len(t): return ""
-        start, end = 0, 0
-        t_count = Counter(t)  # original counter
-        window_count = Counter()  # window counter
-        missing = len(t)  # number of characters missing from the window
+        start = end = 0
+        tc = collections.Counter(t) # original counter
+        wc = collections.Counter()  # window counter
+        missing = len(t)           # number of characters missing from the window
         best = ""
-
+        
         while end < len(s):
             # go right until we have all characters needed
-            while end < len(s) and missing != 0:
-                if s[end] in t_count:
-                    window_count[s[end]] += 1
-                    if window_count[s[end]] <= t_count[s[end]]:
+            while missing != 0 and end<len(s):   
+                if s[end] in tc:
+                    wc[s[end]] += 1
+                    if wc[s[end]] <= tc[s[end]]:
                         missing -= 1
                 end += 1
-
+            
             # remove characters from the left until we don't have enough characters anymore
-            while start < len(s) and missing == 0:
+            while missing == 0 and start < len(s):
+                while s[start] not in tc: # skip irrelevant characters
+                    start += 1
                 # compare to and save best solution
-                if end - start < len(best) or not best:
+                if end-start < len(best) or not best:
                     best = s[start:end]
-
-                if s[start] in window_count:
-                    window_count[s[start]] -= 1
-                    if window_count[s[start]] < t_count[s[start]]:
-                        missing += 1
+                wc[s[start]] -= 1
+                if wc[s[start]] < tc[s[start]]:
+                    missing += 1
                 start += 1
-
         return best
