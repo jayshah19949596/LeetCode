9edad0ee47528d29db51ad40898ccee357b5be39
Jai Shah <jai.s@somaiya.edu>
2024-12-04 21:34:20 -0800

    Update all_nodes_distance_k_in_binary_tree.py

--- 863. All Nodes Distance K in Binary Tree [MEDIUM]/all_nodes_distance_k_in_binary_tree.py
+++ 863. All Nodes Distance K in Binary Tree [MEDIUM]/all_nodes_distance_k_in_binary_tree.py
@@ -13,6 +13,11 @@ Intuition: Traverse from target node and see if the children or ancestrol nodes 
 Find Path from root to target in path.
 For all node in path traverse in dfs fashion to find nodes distanced with value k from target.
 
+Key Ideas:
+1. Path finding: First, the path from the root to the target node is determined.
+2. DFS search: Starting from the target node, it performs DFS to find nodes that are exactly k distance away.
+3. Blockers: When moving up from the target node (along the path), child nodes (blockers) are ignored during further DFS to avoid revisiting nodes.
+
 ### 3. Complexity Analysis:
 ----------------------------
 Time Complexity: O(N): for visiting all nodes
@@ -30,10 +35,11 @@ class TreeNode:
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
         path, results, blocker = [], [], None
-        self.root_target_path(root, target, path)
+        self.root_target_path(root, target, path) # path[0] = target node and path[-1]=root node
         for i in range(len(path)):
+            # blocker is the immediate child node i.e. path[i-1] from current node i.e. path[i]
             if i>0: blocker = path[i-1]
-            self.find_nodes_with_k_dist(path[i], k-i, results, blocker)
+            self.find_nodes_with_k_dist(path[i], k-i, results, blocker) # At current node the kth distance from target node will be (k-i)th distance from current node i.e. path[i]
         return results
 
     def find_nodes_with_k_dist(self, node, dist, results, blocker):
