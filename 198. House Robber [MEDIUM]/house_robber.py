f6b8538b072fb6e08e67eabb2eaf4abe69e857d8
Jai Shah <jai.s@somaiya.edu>
2024-12-05 22:14:54 -0800

    Update house_robber.py

--- 198. House Robber [MEDIUM]/house_robber.py
+++ 198. House Robber [MEDIUM]/house_robber.py
@@ -80,17 +80,16 @@ class Solution:
     Time: O(N)
     Space: O(1)
     """
-class Solution:
-    def rob(self, houses: List[int]) -> int:
-        max_loot_so_far = 0
-        max_loot_till_prev_idx = 0
-        max_loot_till_before_prev_idx = 0
+    def rob(self, houses: List[int]) -> int:        
+        max_rob_so_far = 0
+        max_rob_till_prev_idx = 0
+        max_rob_till_before_prev_idx = 0
 
         for i in range(0, len(houses)):
-            take_current_rob = max_loot_till_before_prev_idx + houses[i]
-            skip_current_rob = max_loot_till_prev_idx
-            max_loot_so_far = max(take_current_rob, skip_current_rob)
-            max_loot_till_before_prev_idx = max_loot_till_prev_idx
-            max_loot_till_prev_idx = max_loot_so_far
+            take_cur_rob = max_rob_till_before_prev_idx + houses[i]
+            skip_cur_rob = max_rob_till_prev_idx
+            max_rob_so_far = max(take_cur_rob, skip_cur_rob)
+            max_rob_till_before_prev_idx = max_rob_till_prev_idx
+            max_rob_till_prev_idx = max_rob_so_far
 
-        return max_loot_so_far
+        return max_rob_so_far
