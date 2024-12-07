5dda9682a70272ffc096108d3b75954eb189aeac
Jai Shah <jai.s@somaiya.edu>
2024-12-07 12:40:11 -0800

    Update first_missing_positive.py

--- 41. First Missing Positive [HARD]/first_missing_positive.py
+++ 41. First Missing Positive [HARD]/first_missing_positive.py
@@ -27,7 +27,7 @@ class Solution:
         i, n = 0, len(nums)
         while i < n:
             correct_idx = nums[i] - 1 # Correct index of the current value is nums[i] - 1
-            # nums[i] != nums[correct_idx] condition id to ignore swapping if the duplicate element is in it's correct index
+            # nums[i] != nums[correct_idx] condition is to ignore swapping if the duplicate element is in it's correct index
             if (0 < nums[i] <= n and nums[i] != nums[correct_idx]):  
                 nums[i], nums[correct_idx] = nums[correct_idx], nums[i] # Swap the current value to it index
             else:
