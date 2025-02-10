a4560c0054feaf57a7218739aa71d370ab132ea1
Jai Shah <jai.s@somaiya.edu>
2025-02-09 19:07:36 -0800

    Update bst_to_sdll.py

--- 426. Convert Binary Search Tree to Sorted Doubly Linked List [MEDIUM]/bst_to_sdll.py
+++ 426. Convert Binary Search Tree to Sorted Doubly Linked List [MEDIUM]/bst_to_sdll.py
@@ -9,6 +9,42 @@ class Node:
 
 class Solution:
     def treeToDoublyList(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root: return
+        # The smallest (first) and the largest (last) nodes
+        first, last = None, None
+
+        def dfs_inorder_helper(node):
+            """
+            Performs standard inorder traversal:
+            left -> node -> right
+            and links all nodes into DLL
+            """
+            if not node: return
+
+            dfs_inorder_helper(node.left)  # left
+
+            nonlocal last, first
+            # if last and node: print("last", last.val, "node", node.val)
+            if last:
+                # link the previous node (last) with the current one (node)
+                last.right = node
+                node.left = last
+            else:
+                first = node # keep the smallest node to close DLL later on
+            last = node
+
+            dfs_inorder_helper(node.right)  # right
+
+        dfs_inorder_helper(root)
+
+        # close DLL
+        last.right = first
+        first.left = last
+        return first
+
+
+class Solution:
+    def treeToDoublyList(self, root: 'Optional[Node]') -> 'Optional[Node]':
         if root is None: return None
 
         dummy_head = prev = Node(0)
