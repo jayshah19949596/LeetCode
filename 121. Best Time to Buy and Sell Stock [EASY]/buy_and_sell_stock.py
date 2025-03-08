2942ec55a365ef55d791fb88e9e66c32ffb4a3ce
Jai Shah <jai.s@somaiya.edu>
2025-03-08 10:20:04 -0800

    Update buy_and_sell_stock.py

--- 121. Best Time to Buy and Sell Stock [EASY]/buy_and_sell_stock.py
+++ 121. Best Time to Buy and Sell Stock [EASY]/buy_and_sell_stock.py
@@ -27,6 +27,6 @@ class Solution:
 
         for price in prices:
             min_price = min(min_price, price)
-            max_profit = max(price - min_price, max_profit)
+            max_profit = max(max_profit, price - min_price)
 
         return max_profit
