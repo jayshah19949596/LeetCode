0251ce2c4c6ad24e26b9597af8c0567b32c41727
Jai Shah <jai.s@somaiya.edu>
2025-03-09 12:01:03 -0700

    Update string_compression.py

--- 443. String Compression [MEDIUM]/string_compression.py
+++ 443. String Compression [MEDIUM]/string_compression.py
@@ -1,3 +1,8 @@
+"""
+----------------------
+Approach 1: Simulation: Write as you go
+----------------------
+"""
 class Solution:
     def compress(self, chars: List[str]) -> int:
         i = anchor_idx = 0
@@ -18,3 +23,27 @@ class Solution:
                     anchor_idx += 1
             i += 1
         return anchor_id
+
+
+"""
+----------------------
+Approach 2: Simulation: Write at terminating condition when chars are not equal 
+----------------------
+"""
+class Solution:
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+        anchor = write = 0
+        for read, c in enumerate(chars):
+            if read + 1 == len(chars) or chars[read + 1] != c:
+                chars[write] = chars[anchor]
+                write += 1
+                if read > anchor:
+                    for digit in str(read - anchor + 1):
+                        chars[write] = digit
+                        write += 1
+                anchor = read + 1
+        return write
