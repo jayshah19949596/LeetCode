a0832266332ec5b84d5f415edd4e0fecad0c9ed3
Jai Shah <jai.s@somaiya.edu>
2025-03-09 14:09:27 -0700

    Create greatest_common_divisor.py

---
+++ 1071. Greatest Common Divisor of Strings [EASY]/greatest_common_divisor.py
@@ -0,0 +1,27 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        len1, len2 = len(str1), len(str2)
+        
+        def valid(base_length):
+            if len1 % k !=0 or len2 % k != 0:  return False
+                
+            n1, n2 = len1 // base_length, len2 // base_length
+            base = str1[:base_length]
+            return str1 == n1 * base and str2 == n2 * base 
+        
+        for i in range(min(len1, len2), 0, -1):
+            if valid(i):
+                return str1[:i]
+        return ""
+
+
+import math
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        # Check if they have non-zero GCD string.
+        if str1 + str2 != str2 + str1:
+            return ""
+
+        # Get the GCD of the two lengths.
+        max_length = math.gcd(len(str1), len(str2))
+        return str1[:max_length]
