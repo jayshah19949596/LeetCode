2157cfa6c247dbdabed2549925d2dc104c7640bd
Jai Shah <jai.s@somaiya.edu>
2025-03-09 21:03:16 -0700

    Update longest_palindromic_substring.py

--- 5. Longest Palindromic Substring [MEDIUM]/longest_palindromic_substring.py
+++ 5. Longest Palindromic Substring [MEDIUM]/longest_palindromic_substring.py
@@ -4,7 +4,7 @@ class Solution:
         
         for center in range(2*len(s)-1):
             left = center//2
-            right = left+(center%2)
+            right = left+(center%2) # For even "center", right=left ELSE right=left+1  
             
             while left>= 0 and right<len(s) and s[left] == s[right]:
                 left, right = left-1, right+1
