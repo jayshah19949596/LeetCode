a1161beb790f1ebfe580cf5dfe312262bd146a30
Jai Shah <jai.s@somaiya.edu>
2025-03-16 21:09:30 -0700

    Create remove_nth_node.py

---
+++ 19. Remove Nth Node From End of List [MEDIUM]/remove_nth_node.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeNthFromEnd(self, head, n):
+        first, second = head, head
+        
+        for i in range(n):
+            first = first.next
+        if not first:
+            return head.next
+        while first.next:
+            first = first.next
+            second = second.next
+            
+        second.next = second.next.next
+        return head
