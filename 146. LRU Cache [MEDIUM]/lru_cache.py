fd8c1f3138305e7fcb33aa77a6b0a91bb79a9d45
Jai Shah <jai.s@somaiya.edu>
2026-01-31 22:23:44 -0800

    Update lru_cache.py
    Simplify LRU

--- 146. LRU Cache [MEDIUM]/lru_cache.py
+++ 146. LRU Cache [MEDIUM]/lru_cache.py
@@ -1,57 +1,46 @@
-class Node(object):
+class Node:
     def __init__(self, key, val):
         self.key = key
         self.val = val
+        self.prev = None
+        self.next = None
 
+class LRUCache:
 
-class DLL(object):
-    def __init__(self):
-        self.head = Node(None, None)
-        self.tail = Node(None, None)
-        self.head.next = self.tail
-        self.tail.prev = self.head
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}
 
-    def insert_node_at_tail(self, node):
-        node.prev = self.tail.prev
-        node.next = self.tail
-        self.tail.prev = node
-        node.prev.next = node
+        self.oldest = Node(0, 0)
+        self.latest = Node(0, 0)
+        self.oldest.next = self.latest
+        self.latest.prev = self.oldest
+        
+    def insert(self, node):
+        prev, next = self.latest.prev, self.latest
+        prev.next = next.prev = node
+        node.next = next
+        node.prev = prev
 
-    def update_node(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
-        self.insert_node_at_tail(node)
+    def remove(self, node):
+        prev, next = node.prev, node.next
+        prev.next = next
+        next.prev = prev
 
-    def remove_node_at_head(self):
-        node = self.head.next
-        self.head.next = node.next
-        node.next.prev = self.head
-        removed_key = node.key
-        del node
-        return removed_key
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
 
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
 
-class LRUCache(object):
-    def __init__(self, capacity):
-        self.mapping = {}
-        self.capacity = capacity
-        self.dll = DLL()
-
-    def get(self, key):
-        if key not in self.mapping:
-            return -1
-        self.dll.update_node(self.mapping[key])
-        return self.mapping[key].val
-
-    def put(self, key, value):
-        if key in self.mapping:
-            node = self.mapping[key]
-            node.val = value
-            self.mapping[key] = node
-            self.dll.update_node(self.mapping[key])
-        else:
-            if len(self.mapping) == self.capacity:
-                removed_key = self.dll.remove_node_at_head()
-                del self.mapping[removed_key]
-            self.mapping[key] = Node(key, value)
-            self.dll.insert_node_at_tail(self.mapping[key])
+        if len(self.cache) > self.cap:
+            lru_node = self.oldest.next
+            self.remove(lru_node)
+            del self.cache[lru_node.key]
