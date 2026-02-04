bc7f96926a11bc41ac63a87ab347dace42254021
Jai Shah <jai.s@somaiya.edu>
2026-02-03 23:35:50 -0800

    Update longes_substr_without_repeat.py

--- 3. Longest Substring Without Repeating Characters [MEDIUM]/longes_substr_without_repeat.py
+++ 3. Longest Substring Without Repeating Characters [MEDIUM]/longes_substr_without_repeat.py
@@ -1,10 +1,13 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        last_seen = defaultdict(int)
+        last_seen = {}
         start = longest = 0
-        substr = ""
         for end, char in enumerate(s):
-            if char in last_seen and start<=last_seen[char]:
+            """
+            2nd condition is to only move "start" if the repeated char is with-in current window else wihtout it start can go backwards.
+            Failure case : s = "abba"
+            """
+            if char in last_seen and start<=last_seen[char]: 
                 start = last_seen[char]+1
             last_seen[char] = end
             longest = max(longest, end-start+1)
