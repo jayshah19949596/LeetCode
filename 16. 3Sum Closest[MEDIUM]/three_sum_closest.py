98d3105b5c14fbb091ac7e566cd088b2b1ba0032
Jai Shah <jai.s@somaiya.edu>
2026-02-05 20:34:02 -0800

    Create three_sum_closest.py

---
+++ 16. 3Sum Closest[MEDIUM]/three_sum_closest.py
@@ -0,0 +1,32 @@
+class Solution:
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        closest = float('inf')
+        nums.sort()
+        i = 0
+        
+        while i < len(nums)-2 :
+            
+            j = i+1
+            k = len(nums)-1
+            
+            while j<k:
+                triple = nums[i] + nums[j] + nums[k]
+                
+                if triple == target:
+                    return triple
+                if abs(triple - target) < abs(closest - target):
+                    closest = triple
+                
+                if triple > target:
+                    k -= 1
+                else:
+                    j += 1
+            i += 1
+        
+        return closest
+                
