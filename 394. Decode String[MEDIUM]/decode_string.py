82784f254bfc0ac34121d096433d65ef68556b79
Jai Shah <jai.s@somaiya.edu>
2026-02-11 01:02:16 -0800

    Update decode_string.py

--- 394. Decode String[MEDIUM]/decode_string.py
+++ 394. Decode String[MEDIUM]/decode_string.py
@@ -1,3 +1,30 @@
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []      # holds (prev_str, repeat_count)
+        curr_str = ""
+        curr_num = 0
+
+        for char in s:
+            if char.isdigit():
+                curr_num = curr_num * 10 + int(char)
+
+            elif char == "[":
+                stack.append((curr_str, curr_num))
+                curr_str = ""
+                curr_num = 0
+
+            elif char == "]":
+                prev_str, prev_num = stack.pop()
+                curr_str = prev_str + curr_str * prev_num
+
+            else:
+                curr_str += char
+
+        return curr_str
+
+
+
 class Solution:
     def decodeString(self, encoded_string):
         """
