9f07de1ee3c9b99dfb91fbb935f4572c7e2ee875
Jai Shah <jai.s@somaiya.edu>
2026-02-11 23:40:24 -0800

    Update majority_element.py

--- 169. Majority Element [EASY]/majority_element.py
+++ 169. Majority Element [EASY]/majority_element.py
@@ -1,15 +1,33 @@
 class Solution:
     def majorityElement(self, nums):
+        # Boyer-Moore Voting Algorithm
+        # Idea: If we pair each occurrence of the majority element with a different element, 
+        # then majority element will still remain at the end because it appears > n/2 times.
+
         count = 0
         candidate = None
+
         for num in nums:
+
+            # If count drops to 0, choose current number as a new candidate.
+            # This means previous candidate has been "balanced out".
             if count == 0:
                 candidate = num
-            if num == candidate: 
-              count += 1
-              if count == len(nums)/2: return candidate
-            else: count -=1
+
+            # If current number matches candidate, increase vote count.
+            if num == candidate:
+                count += 1
+                # If candidate already appears more than half then return it.
+                if count == len(nums) / 2:
+                    return candidate
+            else:
+                # Otherwise, cancel out one vote.
+                count -= 1
+
+        # At the end, candidate holds the majority element
+        # because majority element always survives cancellation.
         return candidate
+
 
 
 from collections import Counter
