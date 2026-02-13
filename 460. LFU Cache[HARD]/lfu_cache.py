3f4b302ea4a03fdea7a16800b715cf60d33edd78
Jai Shah <jai.s@somaiya.edu>
2026-02-12 18:10:32 -0800

    Update lfu_cache.py

--- 460. LFU Cache[HARD]/lfu_cache.py
+++ 460. LFU Cache[HARD]/lfu_cache.py
@@ -2,29 +2,29 @@ from collections import defaultdict, OrderedDict
 
 class LFUCache:
     def __init__(self, capacity: int):
-        self.capacity = capacity
+        self.cap = capacity
         self.key_to_freq = defaultdict(int)  # key -> (frequency, value)
         self.freq_to_key = defaultdict(OrderedDict)  # frequency -> OrderedDict(key -> None)
         self.min_freq = 0
 
-    def _insert(self, key, frequency, value):
+    def _insert(self, key, freq, value):
         """Insert key into a specific frequency bucket."""
-        self.key_to_freq[key] = (frequency, value)
-        self.freq_to_key[frequency][key] = None
+        self.key_to_freq[key] = (freq, value)
+        self.freq_to_key[freq][key] = None
 
     def _increase_frequency(self, key):
         """Increase frequency of a key and move it to the next frequency bucket."""
-        frequency, value = self.key_to_freq[key]
+        freq, value = self.key_to_freq[key]
 
         # Remove from current frequency bucket
-        del self.freq_to_key[frequency][key]
+        del self.freq_to_key[freq][key]
 
         # If this was the last key in the minimum frequency bucket, update min_frequency
-        if frequency == self.min_freq and not self.freq_to_key[frequency]:
+        if freq == self.min_freq and not self.freq_to_key[freq]:
             self.min_freq += 1
 
         # Insert into next frequency bucket
-        self._insert(key, frequency + 1, value)
+        self._insert(key, freq + 1, value)
         return value
 
     def get(self, key: int) -> int:
@@ -34,18 +34,15 @@ class LFUCache:
 
     def put(self, key: int, value: int) -> None:
 
-        if self.capacity == 0:
-            return
-
         # If key exists → update value + bump frequency
         if key in self.key_to_freq:
             self._increase_frequency(key)
-            frequency, _ = self.key_to_freq[key]
-            self.key_to_freq[key] = (frequency, value)
+            freq, _ = self.key_to_freq[key]
+            self.key_to_freq[key] = (freq, value)
             return
 
         # Evict if at capacity
-        if len(self.key_to_freq) == self.capacity:
+        if len(self.key_to_freq) == self.cap:
             evict_key, _ = self.freq_to_key[self.min_freq].popitem(last=False)
             del self.key_to_freq[evict_key]
 
