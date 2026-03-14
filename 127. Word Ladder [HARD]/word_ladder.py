647955d3e86bca38b300bd6f5ee1b12d953bfec5
Jai Shah <jai.s@somaiya.edu>
2026-03-14 01:09:02 -0700

    Update word_ladder.py

--- 127. Word Ladder [HARD]/word_ladder.py
+++ 127. Word Ladder [HARD]/word_ladder.py
@@ -27,3 +27,28 @@ class Solution:
                         wordSet.remove(newWord)  # Remove from wordSet to prevent revisits
 
         return 0  # No transformation sequence found
+
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if len(beginWord) != len(endWord): return 0
+        word_set = set(wordList)
+        for word in wordList:
+            if len(word) == len(beginWord):
+                word_set.add(word)
+        if len(word_set) == 0: return 0
+        if endWord not in word_set: return 0
+
+        queue = deque([[beginWord, 1]])
+        visited = set([beginWord])
+        while queue:
+            cur_word, cur_dist = queue.pop()
+            if cur_word == endWord: return cur_dist
+            for nxt_word in word_set:
+                diff = 0
+                for i in range(len(cur_word)):
+                    if cur_word[i] != nxt_word[i]: diff += 1
+                if diff == 1 and nxt_word not in visited:
+                    visited.add(nxt_word)
+                    queue.appendleft([nxt_word, cur_dist+1])
+        return 0
