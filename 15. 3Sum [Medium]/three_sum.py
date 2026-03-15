ea7ffe0fc1994b8f90b9133f7586d202e800ac55
Jai Shah <jai.s@somaiya.edu>
2026-03-15 01:23:18 -0700

    Update three_sum.py

--- 15. 3Sum [Medium]/three_sum.py
+++ 15. 3Sum [Medium]/three_sum.py
@@ -18,34 +18,30 @@ Space Complexity: O(N)
 from typing import List
 
 
-class Solution(object):
+
+class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        i = target = 0
-        results = []
         nums.sort()
-        while i < len(nums):
+        result = []
+
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]: continue
+
             left, right = i + 1, len(nums) - 1
 
             while left < right:
-                triplet_sum = nums[i] + nums[left] + nums[right]
-                if triplet_sum == target:
-                    results.append([nums[i], nums[left], nums[right]])
-                    left, right = left+1, right-1
-                    while nums[right] == nums[right + 1] and right > left:
-                        right -= 1
-                    while nums[left] == nums[left - 1] and left < right:
-                        left += 1
-                elif triplet_sum < target:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total < 0: left += 1
+                elif total > 0: right -= 1
+                else:
+                    result.append([nums[i], nums[left], nums[right]])
                     left += 1
-                    while nums[left] == nums[left - 1] and left < right:
-                        left += 1
-                elif triplet_sum > target:
                     right -= 1
-                    while nums[right] == nums[right + 1] and right > left:
+
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
                         right -= 1
 
-            i += 1
-            while i < len(nums) - 2 and nums[i] == nums[i - 1]:
-                i += 1
-
-        return results
+        return result
