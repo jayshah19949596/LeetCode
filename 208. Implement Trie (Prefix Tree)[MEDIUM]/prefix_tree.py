9ce8fa3c202bc7c4fc883eeac8ab1cfe54212461
Jai Shah <jai.s@somaiya.edu>
2026-03-14 21:38:55 -0700

    Update prefix_tree.py

--- 208. Implement Trie (Prefix Tree)[MEDIUM]/prefix_tree.py
+++ 208. Implement Trie (Prefix Tree)[MEDIUM]/prefix_tree.py
@@ -2,7 +2,7 @@ class Trie:
 
     def __init__(self):
         self.trie = {}
-        self.is_end = False
+        self.END = "#"
 
     def insert(self, word: str) -> None:
         node = self.trie
@@ -10,7 +10,7 @@ class Trie:
             if char not in node:
                 node[char] = {}
             node = node[char]
-        node[self.is_end] = True
+        node[self.END] = True
         
 
     def search(self, word: str) -> bool:
@@ -19,7 +19,7 @@ class Trie:
             if char not in node:
                 return False
             node = node[char]
-        return self.is_end in node
+        return self.END in node
 
     def startsWith(self, prefix: str) -> bool:
         node = self.trie
