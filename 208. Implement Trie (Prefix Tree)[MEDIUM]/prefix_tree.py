b56d862afbd65ec252653a7c5010ceb9536bff75
Jai Shah <jai.s@somaiya.edu>
2026-03-14 21:36:43 -0700

    Update prefix_tree.py

--- 208. Implement Trie (Prefix Tree)[MEDIUM]/prefix_tree.py
+++ 208. Implement Trie (Prefix Tree)[MEDIUM]/prefix_tree.py
@@ -1,32 +1,19 @@
 class Trie:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.trie = {}
         self.is_end = False
-        
 
-    def insert(self, word):
-        """
-        Inserts a word into the trie.
-        :type word: str
-        :rtype: void
-        """
+    def insert(self, word: str) -> None:
         node = self.trie
         for char in word:
             if char not in node:
                 node[char] = {}
             node = node[char]
         node[self.is_end] = True
+        
 
-    def search(self, word):
-        """
-        Returns if the word is in the trie.
-        :type word: str
-        :rtype: bool
-        """
+    def search(self, word: str) -> bool:
         node = self.trie
         for char in word:
             if char not in node:
@@ -34,12 +21,7 @@ class Trie:
             node = node[char]
         return self.is_end in node
 
-    def startsWith(self, prefix):
-        """
-        Returns if there is any word in the trie that starts with the given prefix.
-        :type prefix: str
-        :rtype: bool
-        """
+    def startsWith(self, prefix: str) -> bool:
         node = self.trie
         for char in prefix:
             if char not in node:
