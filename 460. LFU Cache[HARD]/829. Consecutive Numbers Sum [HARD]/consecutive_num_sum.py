6cab58f3e75058dd9c3e72a1d3ed5f5310dccd59
Jai Shah <jai.s@somaiya.edu>
2026-03-15 22:51:06 -0700

    Create consecutive_num_sum.py

---
+++ 460. LFU Cache[HARD]/829. Consecutive Numbers Sum [HARD]/consecutive_num_sum.py
@@ -0,0 +1,19 @@
+"""
+We are essentially checking every possible consecutive sequence that could sum to n.
+This brute force works but is slow
+"""
+class Solution:
+    def consecutiveNumbersSum(self, n: int) -> int:
+        answer = 1   # n itself always counts
+
+        for start in range(1, n):
+            total_sum = 0
+            for num in range(start, n):
+                total_sum += num
+                if total_sum == n:
+                    answer += 1
+                    break
+                elif total_sum > n:
+                    break
+
+        return answer
