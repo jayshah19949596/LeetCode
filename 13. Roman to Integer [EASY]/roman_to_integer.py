dd2a03d703881fff76f210ce5893962e94bc0134
Jai Shah <jai.s@somaiya.edu>
2026-03-20 23:37:40 -0700

    Update roman_to_integer.py

--- 13. Roman to Integer [EASY]/roman_to_integer.py
+++ 13. Roman to Integer [EASY]/roman_to_integer.py
@@ -26,3 +26,24 @@ class Solution:
                 i += 1
 
         return integer
+
+
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        total = 0
+        i = 0
+        while i < len(s):
+            # When a smaller valued symbol is before a larger valued symbol
+            # If this is the subtractive case.
+            if i + 1 < len(s) and values[s[i]] < values[s[i + 1]]:
+                total += values[s[i + 1]] - values[s[i]]
+                i += 2
+            # Else this is NOT the subtractive case.
+            else:
+                total += values[s[i]]
+                i += 1
+        return total
+
