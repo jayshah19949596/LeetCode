ab44e14702c33ff601aecd13483de101a26df8d9
Jai Shah <jai.s@somaiya.edu>
2026-03-20 20:17:44 -0700

    Update merge_sorted_array.py

--- 88. Merge Sorted Array [EASY]/merge_sorted_array.py
+++ 88. Merge Sorted Array [EASY]/merge_sorted_array.py
@@ -4,13 +4,10 @@ https://leetcode.com/problems/merge-sorted-array
 """
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
         i = m-1
         j = n-1
         k = m+n-1
-
+        # Start merging from right side
         while i >= 0 and j >= 0:
             if nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
