bd749832a535431efb8a972acffde5055c0e70b3
Jai Shah <jai.s@somaiya.edu>
2026-03-22 13:35:50 -0700

    Update spiral_matrix.py

--- 54. Spiral Matrix [MEDIUM]/spiral_matrix.py
+++ 54. Spiral Matrix [MEDIUM]/spiral_matrix.py
@@ -1,25 +1,25 @@
 class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        start_row, end_row = 0, len(matrix)
-        start_col, end_col = 0, len(matrix[0])
+    def spiralOrder(self, mat: List[List[int]]) -> List[int]:
+        start_row, end_row = 0, len(mat)
+        start_col, end_col = 0, len(mat[0])
         results = []
         while start_row<end_row and start_col<end_col:
-            for i in range(start_col, end_col):
-                results.append(matrix[start_row][i])
+            for col in range(start_col, end_col):
+                results.append(mat[start_row][col])
             start_row += 1
-            
-            for i in range(start_row, end_row):
-                results.append(matrix[i][end_col-1])
+
+            for row in range(start_row, end_row):
+                results.append(mat[row][end_col-1])
             end_col -= 1
-            
+
             if end_row>start_row:
-                for i in range(end_col, start_col, -1):
-                    results.append(matrix[end_row-1][i-1])
+                for col in range(end_col, start_col, -1):
+                    results.append(mat[end_row-1][col-1])
                 end_row -= 1
-            
+
             if end_col>start_col:
-                for i in range(end_row, start_row, -1):
-                    results.append(matrix[i-1][start_col])
+                for row in range(end_row, start_row, -1):
+                    results.append(mat[row-1][start_col])
                 start_col += 1
+
         return results
-            
