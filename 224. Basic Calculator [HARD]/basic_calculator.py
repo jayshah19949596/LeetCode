2b90509c5792337f3349f6642080b1fbeda1d19a
Jai Shah <jai.s@somaiya.edu>
2026-03-23 13:47:48 -0700

    Update basic_calculator.py

--- 224. Basic Calculator [HARD]/basic_calculator.py
+++ 224. Basic Calculator [HARD]/basic_calculator.py
@@ -18,6 +18,45 @@ Output: 23
 
 class Solution:
     def calculate(self, s: str) -> int:
+        # Stack will store [previous_total, previous_sign]
+        stack = []
+        num, sign = 0, 1
+        ans = 0
+        s = s + "+" # We add a '+' at the end to "flush" the final number into the total
+        
+        for char in s:
+            if char.isdigit():
+                # Correctly build multi-digit numbers (e.g., "42")
+                num = num * 10 + int(char)
+            
+            elif char in "+-":
+                # Finalize the current number before moving to the next sign
+                ans += sign * num
+                # Update sign for the NEXT number
+                sign = 1 if char == "+" else -1
+                num = 0
+                
+            elif char == "(":
+                # Pushing a nested list: [current_total, current_sign]
+                stack.append([ans, sign])
+                # Reset for the expression inside the parentheses
+                ans, sign = 0, 1
+                
+            elif char == ")":
+                # Finalize the last number inside the parentheses
+                ans += sign * num
+                num = 0
+                
+                # Retrieve the nested list [prev_ans, prev_sign]
+                prev_ans, prev_sign = stack.pop()
+                
+                # Result = (Inside_Result * sign_before_paren) + ans_before_paren
+                ans = prev_ans + (prev_sign * ans)
+                
+        return ans
+        
+class Solution:
+    def calculate(self, s: str) -> int:
 
         stack = []
         operand = 0
