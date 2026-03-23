12b66682861d34be09fab0df25734f3e6c3e6682
Jai Shah <jai.s@somaiya.edu>
2026-03-23 13:48:49 -0700

    Update basic_calculator.py

--- 224. Basic Calculator [HARD]/basic_calculator.py
+++ 224. Basic Calculator [HARD]/basic_calculator.py
@@ -20,7 +20,7 @@ class Solution:
     def calculate(self, s: str) -> int:
         # Stack will store [previous_total, previous_sign]
         stack = []
-        num, sign = 0, 1
+        num, sign = 0, 1 # 1 means positive, -1 means negative  
         ans = 0
         s = s + "+" # We add a '+' at the end to "flush" the final number into the total
         
@@ -54,59 +54,4 @@ class Solution:
                 ans = prev_ans + (prev_sign * ans)
                 
         return ans
-        
-class Solution:
-    def calculate(self, s: str) -> int:
-
-        stack = []
-        operand = 0
-        res = 0 # For the on-going result
-        sign = 1 # 1 means positive, -1 means negative  
-
-        for ch in s:
-            if ch.isdigit():
-
-                # Forming operand, since it could be more than one digit
-                operand = (operand * 10) + int(ch)
-
-            elif ch == '+':
-
-                # Evaluate the expression to the left, with result, sign, operand
-                res += sign * operand
-                # Save the recently encountered '+' sign
-                sign = 1
-                # Reset operand
-                operand = 0
-
-            elif ch == '-':
-
-                res += sign * operand
-                sign = -1
-                operand = 0
-
-            elif ch == '(':
-
-                # Push the result and sign on to the stack, for later We push the result first, then sign
-                stack.append(res)
-                stack.append(sign)
-
-                # Reset operand and result, as if new evaluation begins for the new sub-expression
-                sign = 1
-                res = 0
-
-            elif ch == ')':
-
-                # Evaluate the expression to the left with result, sign and operand
-                res += sign * operand
-
-                # ')' marks end of expression within a set of parenthesis 
-                # Its result is multiplied with sign on top of stack as stack.pop() is the sign before the parenthesis
-                res *= stack.pop() # stack pop 1, sign
-
-                # Then add to the next operand on the top.
-                # As stack.pop() is the result calculated before this parenthesis (operand on stack) + (sign on stack * (result from parenthesis))
-                res += stack.pop() # stack pop 2, operand
-                
-                operand = 0  # Reset the operand
-
-        return res + sign * operand
+     
