d490fa0ad8fa85f057732fdfda6d793f1488e675
Jai Shah <jai.s@somaiya.edu>
2026-05-10 01:47:03 -0700

    Update pascals_triangle.py

--- 118. Pascal's Triangle [EASY]/pascals_triangle.py
+++ 118. Pascal's Triangle [EASY]/pascals_triangle.py
@@ -17,7 +17,22 @@ Space - O(N**2)
 """
 from typing import List
 
-
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 0: return [] # Handle edge case
+        
+        res = [[1]]
+        for row in range(1, numRows):
+            no_of_ele = row + 1
+            cur_row = [1]
+            # Use the previous row in res to calculate middle elements
+            for j in range(no_of_ele - 2):
+                cur_row.append(res[-1][j] + res[-1][j+1])
+            cur_row.append(1)
+            res.append(cur_row) 
+            
+        return res
+        
 class Solution:
     def generate(self, num_rows: int) -> List[List[int]]:
         if num_rows == 0:
