7eeda45f556d06f90e5a3678f8c290c666e43159
Jai Shah <jai.s@somaiya.edu>
2026-05-10 23:33:29 -0700

    Update median_of_two_sorted_arrays.py

--- 4. Median of Two Sorted Arrays [HARD]/median_of_two_sorted_arrays.py
+++ 4. Median of Two Sorted Arrays [HARD]/median_of_two_sorted_arrays.py
@@ -19,31 +19,32 @@ class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         if len(nums1)>len(nums2): nums1, nums2 = nums2, nums1
         x, y, = len(nums1), len(nums2)
+        full_len = x+y
         low, high = 0, len(nums1)
-        half_len = (x+y+1)//2
+        half_len = (full_len+1)//2
         while low<=high:
-            partition_x = (low+high)//2 # nums1[partition_x] is the first element of the right half
-            partition_y = half_len-partition_x  # nums2[partition_y] is the first element of the right half 
+            pivot_x = (low+high)//2 # nums1[pivot_x] is the first element of the right half
+            pivot_y = half_len-pivot_x  # nums2[pivot_y] is the first element of the right half 
 
-            if partition_x-1 < 0: max_left_x = -float("inf")
-            else: max_left_x = nums1[partition_x-1]
+            if pivot_x-1 < 0: max_left_x = -float("inf")
+            else: max_left_x = nums1[pivot_x-1]
 
-            if partition_x == x: min_right_x = float("inf")
-            else: min_right_x = nums1[partition_x]
+            if pivot_x == x: min_right_x = float("inf")
+            else: min_right_x = nums1[pivot_x]
 
-            if partition_y-1 < 0: max_left_y = -float("inf")
-            else: max_left_y = nums2[partition_y-1]
+            if pivot_y-1 < 0: max_left_y = -float("inf")
+            else: max_left_y = nums2[pivot_y-1]
 
-            if partition_y == y:  min_right_y = float("inf")
-            else: min_right_y = nums2[partition_y]
+            if pivot_y == y:  min_right_y = float("inf")
+            else: min_right_y = nums2[pivot_y]
 
             results = max(max_left_x, max_left_y)
             if max_left_x<=min_right_y and max_left_y<=min_right_x:
-                if (x+y)%2 == 1:
+                if full_len%2 == 1:
                     return results
                 else:
                     return (results + min(min_right_x, min_right_y))/2
             elif max_left_x>min_right_y:
-                high = partition_x-1
+                high = pivot_x-1
             else:
-                low = partition_x+1
+                low = pivot_x+1
