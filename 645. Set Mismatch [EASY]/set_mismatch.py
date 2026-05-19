ef25fe6723fda58cd9a512116350bfef791ff1c3
Jai Shah <jai.s@somaiya.edu>
2026-05-19 15:18:06 -0700

    Update set_mismatch.py

--- 645. Set Mismatch [EASY]/set_mismatch.py
+++ 645. Set Mismatch [EASY]/set_mismatch.py
@@ -1,3 +1,8 @@
+"""
+Optimized Approach: In Place visit makring 
+Time Complexity: O(n) — We pass through the array exactly twice.
+Space Complexity: O(1) — We reuse the input array as our state machine store, saving heap memory allocations and maximizing CPU cache locality.
+"""
 from typing import List
 
 class Solution:
@@ -6,9 +11,34 @@ class Solution:
         dup = miss = -1
         
         for num in nums:
+            act_num = abs(num)                
+            if nums[act_num - 1] < 0:
+                dup = act_num 
+            else:
+                nums[act_num - 1] = -nums[act_num - 1]  # Mark it as visited
+                
+        for i, num in enumerate(nums):
+            if num>0:
+                miss = i+1
+                break
+        return [dup, miss]
+        
+from typing import List
+
+"""
+Space suboptimized approach: Additional Array to keep track of visited
+Time Complexity: O(n) — We pass through the array exactly twice.
+Space Complexity: O(n) — We need additional input array visited.
+"""
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        visited = [-1] * len(nums)
+        dup = miss = -1
+        
+        for num in nums:
             if seen[num - 1] != -1:
                 dup = num  
-            seen[num - 1] = 1  
+            visited[num - 1] = 1  
                 
         for i, val in enumerate(seen):
             if val == -1:
